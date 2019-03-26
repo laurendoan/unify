7f2824f8966df4ecdb393d357513745d96f540de
Priya Patel <priyap@utexas.edu>
2019-03-26 11:06:02 -0500

    persistant login

--- unify/unify/ViewController.swift
+++ unify/unify/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if Auth.auth().currentUser != nil {
+            // User is signed in.
+            // ...
+            self.performSegue(withIdentifier: "yesUserSegueIdentifier", sender: self)
+        } else {
+            // No user is signed in.
+            // ...
+            self.performSegue(withIdentifier: "noUserSegueIdentifier", sender: self)
+        }
     }
 
 
