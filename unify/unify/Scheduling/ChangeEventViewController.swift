a97652984cdaa89235a9c43236cfa01bd9fd13b8
Lauren Doan <laurendoan@Laurens-MacBook-Pro-2.local>
2019-05-04 21:52:17 -0500

    implemented calendar notifications for updating an event

--- unify/unify/Scheduling/ChangeEventViewController.swift
+++ unify/unify/Scheduling/ChangeEventViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import Firebase
+import UserNotifications
 
 class ChangeEventViewController: UIViewController {
     /* Initialized Variables */
     var contentHolder: EventContent! = nil
     let formatter = DateFormatter()
-    var ref: DatabaseReference!
+    var ref: DatabaseReference! // Database reference.
+    let center = UNUserNotificationCenter.current() // Notification center.
     
     /* Initialized textfields */
     @IBOutlet weak var name: UITextField!
@@ -22,61 +24,11 @@ class ChangeEventViewController: UIViewController {
     @IBOutlet weak var start: UITextField!
     @IBOutlet weak var end: UITextField!
     
-    /* Initialized Action Buttons */
-    @IBAction func saveButton(_ sender: Any) {
-        /* Checks for completed conditions: completed textfields
-         and correct formts for name, location, date, start, and end times */
-        if ((name.text?.isEmpty)! || (location.text?.isEmpty)! ||
-            (date.text?.isEmpty)! || (start.text?.isEmpty)! || (end.text?.isEmpty)!) {
-            alertErrorHelper(text: "ERROR: Incompleted information. Please complete all five textfields.")
-        } else if (formatChecker(condition: "MM/dd/yyyy", text: date.text!) == false
-            || date.text!.count != 10) {
-            alertErrorHelper(text: "ERROR: Incorrect format for Date. (MM/DD/YYYY)")
-        } else if (formatChecker(condition: "HH:mma", text: start.text!) == false
-            || start.text!.count != 7) {
-            alertErrorHelper(text: "ERROR: Incorrect format for Start Time. (EX: 01:30am)")
-        } else if (formatChecker(condition: "HH:mma", text: end.text!) == false
-            || end.text!.count != 7) {
-            alertErrorHelper(text: "ERROR: Incorrect format for End Time. (EX: 01:30pm)")
-        } else {
-            // Readys the textfields for usage
-            let n = name.text!
-            let l = location.text!
-            let d = contentHolder.date!
-            let s = start.text!
-            let e = end.text!
-            
-            // Update data onto Firebase
-            self.ref.child("schedule").child(contentHolder.courseRef!).child(d)
-                    .child(contentHolder.parentRef!).setValue([
-                "date" : d,
-                "location" : l,
-                "name" : n,
-                "start" : s,
-                "end" : e
-                ])
-        }
-        resetAndBack()
-    }
-    
-    /* Deletes the event using the parentIDRef*/
-    @IBAction func deleteButton(_ sender: Any) {
-        self.ref.child("schedule").child(contentHolder.courseRef!).child(contentHolder.date!)
-            .child(contentHolder.parentRef!).removeValue()
-        resetAndBack()
-    }
-    
-    /* Does nothing, no changes are made. Pops back to previous view controller */
-    @IBAction func cancelButton(_ sender: Any) {
-        resetAndBack()
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
         ref = Database.database().reference()
+        
         setup()
     }
     
@@ -113,6 +65,76 @@ class ChangeEventViewController: UIViewController {
         }
     }
     
+    /* Initialized Action Buttons */
+    @IBAction func saveButton(_ sender: Any) {
+        /* Checks for completed conditions: completed textfields
+         and correct formts for name, location, date, start, and end times */
+        if ((name.text?.isEmpty)! || (location.text?.isEmpty)! ||
+            (date.text?.isEmpty)! || (start.text?.isEmpty)! || (end.text?.isEmpty)!) {
+            alertErrorHelper(text: "ERROR: Incompleted information. Please complete all five textfields.")
+        } else if (formatChecker(condition: "MM/dd/yyyy", text: date.text!) == false
+            || date.text!.count != 10) {
+            alertErrorHelper(text: "ERROR: Incorrect format for Date. (MM/DD/YYYY)")
+        } else if (formatChecker(condition: "HH:mma", text: start.text!) == false
+            || start.text!.count != 7) {
+            alertErrorHelper(text: "ERROR: Incorrect format for Start Time. (EX: 01:30am)")
+        } else if (formatChecker(condition: "HH:mma", text: end.text!) == false
+            || end.text!.count != 7) {
+            alertErrorHelper(text: "ERROR: Incorrect format for End Time. (EX: 01:30pm)")
+        } else {
+            // Readys the textfields for usage
+            let n = name.text!
+            let l = location.text!
+            let d = contentHolder.date!
+            let s = start.text!
+            let e = end.text!
+            
+            // Update data onto Firebase
+            self.ref.child("schedule").child(contentHolder.courseRef!).child(d)
+                    .child(contentHolder.parentRef!).setValue([
+                "date" : d,
+                "location" : l,
+                "name" : n,
+                "start" : s,
+                "end" : e
+                ])
+            
+            // Create notification.
+            let notification = UNMutableNotificationContent()
+            notification.title = contentHolder.course!
+            notification.subtitle = n
+            notification.body = "This event has been updated."
+            
+            // Trigger the notification after 3 seconds.
+            let delay: TimeInterval = 3.0
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: delay, repeats: false)
+            
+            // Create request to submit notification.
+            let request = UNNotificationRequest(identifier: "notification", content: notification, trigger: trigger)
+            
+            // Submit request.
+            center.add(request) { error in
+                if let e = error {
+                    print("Add request error: \(e)")
+                }
+            }
+        }
+        
+        resetAndBack()
+    }
+    
+    /* Deletes the event using the parentIDRef*/
+    @IBAction func deleteButton(_ sender: Any) {
+        self.ref.child("schedule").child(contentHolder.courseRef!).child(contentHolder.date!)
+            .child(contentHolder.parentRef!).removeValue()
+        resetAndBack()
+    }
+    
+    /* Does nothing, no changes are made. Pops back to previous view controller */
+    @IBAction func cancelButton(_ sender: Any) {
+        resetAndBack()
+    }
+    
     /* Helper function reset variables to dismiss the VC */
     func resetAndBack() {
         // Resets the variables back to ni
@@ -141,15 +163,4 @@ class ChangeEventViewController: UIViewController {
         alertController.addAction(defaultAction)
         self.present(alertController, animated: true, completion: nil)
     }
-        
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
