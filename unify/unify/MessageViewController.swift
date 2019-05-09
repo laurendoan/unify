e4a5c58af01e0024831c6c23952fecbf66b9938b
Lauren Doan <laurendoan@Laurens-MacBook-Pro-2.local>
2019-05-08 23:28:51 -0500

    updated comments

--- unify/unify/MessageViewController.swift
+++ unify/unify/MessageViewController.swift
@@ -14,8 +14,7 @@ import Firebase
 import UserNotifications
 
 final class MessageViewController: MessagesViewController, MembersDelegate, NotesDelegate, LeaveClassProtocol, UNUserNotificationCenterDelegate {
-    // Database reference.
-    var ref = Database.database().reference()
+    var ref = Database.database().reference() // Database reference.
     let user = Auth.auth().currentUser // Current user.
     
     // Instance variables for messages.
@@ -32,7 +31,7 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
     var membersView = MembersViewController()
     var notesView = NotesViewController()
     var panelOut = false
-    var panelState = -1 //-1: inside, 0: standard, 1: members
+    var panelState = -1 // -1: inside, 0: standard, 1: members.
     
     let center = UNUserNotificationCenter.current() // Notification center.
     let chatAlerts = UserDefaults.standard.bool(forKey: "Chat Alerts") // Chat alerts switch.
@@ -66,93 +65,102 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
         loadMessages()
         setupPanel()
         center.delegate = self
-        self.messagesCollectionView.scrollToBottom(animated: true)
+        self.messagesCollectionView.scrollToBottom(animated: true) // Auto-scroll messages to bottom of screen.
     }
 
     // Sets up side panel.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // Show navigation bar.
         navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        // Customize navigation bar.
         navigationController?.navigationBar.barTintColor = JDColor.appTabBarBackground.color
         navigationController?.navigationBar.tintColor = JDColor.appSubText.color
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:JDColor.appAccent.color]
+        
         if let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout {
             layout.collectionView!.backgroundColor = JDColor.appViewBackground.color
         }
+        
+        // Customize message input bar.
         messageInputBar.contentView.backgroundColor = JDColor.appTabBarBackground.color
         messageInputBar.backgroundView.backgroundColor = JDColor.appTabBarBackground.color
         messageInputBar.inputTextView.textColor = JDColor.appSubText.color
-        self.messagesCollectionView.scrollToBottom(animated: true)
+        
+        self.messagesCollectionView.scrollToBottom(animated: true) // Auto-scroll messages to bottom of screen.
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        // Hide navigation bar.
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     // Sets up side panel.
     func setupPanel() {
-        //Currently, two things can be viewed in panel view: the standard panel class info page, and the members page.
-        //At the start, these are both instantiated and placed with their left edges to the far right of the screen.
+        // Currently, two things can be viewed in panel view: the standard panel class info page, and the members page.
+        // At the start, these are both instantiated and placed with their left edges to the far right of the screen.
         
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "=", style: .plain, target: self, action: #selector(togglePanel)) //first create a button for the panel
-        
+        // Create side panel icon.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "=", style: .plain, target: self, action: #selector(togglePanel))
         self.navigationItem.rightBarButtonItem?.image = UIImage(named: "icons8-menu-26.png")
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
 
-        
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        panelView = storyboard.instantiateViewController(withIdentifier: "PanelViewController") as! PanelViewController
-    
-        //addChild(panelView)
-        panelView.view.frame = CGRect(x: self.view.frame.width/3, y: 0, width: self.view.frame.width*2/3, height: self.view.frame.height) //want it 1/3 of the way across the screen so it's coming from the right
         
+        // Instantiate panel VC.
+        panelView = storyboard.instantiateViewController(withIdentifier: "PanelViewController") as! PanelViewController
+        panelView.view.frame = CGRect(x: self.view.frame.width/3, y: 0, width: self.view.frame.width*2/3, height: self.view.frame.height)
         panelView.tableView.rowHeight = 40
         panelView.tableView.frame = CGRect(x: 0, y: 100, width: 276, height: panelView.tableView.rowHeight * 5)
         
+        
         panelView.delegate = self
         panelView.notesDelegate = self
+        panelView.title = classID
         panelView.classNameRef = className
         panelView.classId = classID
         panelOut = false
+        panelView.leaveClassDelegate = self
+        panelView.className = className  // Pass in class name.
         
-        
+        // Instantiate notes VC.
         notesView = storyboard.instantiateViewController(withIdentifier: "NotesViewController") as! NotesViewController
         notesView.view.frame = CGRect(x: self.view.frame.width/3, y: 0, width: self.view.frame.width*2/3, height: self.view.frame.height) //want it 1/3 of the way across the screen so it's coming from the right
         notesView.className = className
         notesView.classId = classID
         
-        
+        // Instantiate members VC.
         membersView = storyboard.instantiateViewController(withIdentifier: "MembersViewController") as! MembersViewController
         membersView.view.frame = CGRect(x: self.view.frame.width/3, y: 0, width: self.view.frame.width*2/3, height: self.view.frame.height) //want it 1/3 of the way across the screen so it's coming from the right
         membersView.className = self.className
         
+        // Create new navigation controller for side panel.
         navController = UINavigationController(rootViewController: panelView)
-        navController.view.frame = CGRect(x: self.view.frame.width/3, y: 0, width: self.view.frame.width*2/3, height: self.view.frame.height) //want it 1/3 of the way across the screen so it's coming from the right
+        navController.view.frame = CGRect(x: self.view.frame.width/3, y: 0, width: self.view.frame.width*2/3, height: self.view.frame.height)
         navController.navigationItem.title = className
         navController.interactivePopGestureRecognizer?.isEnabled = false
-        panelView.title = classID
         
         addChild(navController)
         self.view.insertSubview(navController.view, at: 0)
         self.view.bringSubviewToFront(navController.view)
-        navController.view.isHidden = true; //don't show it initially
+        navController.view.isHidden = true; // Hide initially.
         navController.view.frame = CGRect(x: self.view.frame.width, y: navController.view.frame.minY, width: navController.view.frame.width, height: navController.view.frame.height)
-        navController.didMove(toParent: self) //the only thing added to the current VC is the nav controller because it will handle the rest
-
-                
-        panelView.leaveClassDelegate = self
-        panelView.className = className  // Pass in class name.
+        navController.didMove(toParent: self) // The only thing added to the current VC is the nav controller because it will handle the rest.
     }
     
-
+    // Toggles side panel when right gesture swipe triggered.
     @IBAction func swipeRight(_ sender: Any) {
         togglePanel()
     }
     
+    // Toggles side panel when left gesture swipe triggered.
     @IBAction func swipeLeft(_ sender: Any) {
         togglePanel()
     }
     
+    // Toggles side panel.
     @objc func togglePanel() {
         if(panelOut == false) {
             navController.view.isHidden = false
@@ -162,8 +170,7 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
             UIView.animate(withDuration: 0.3, animations: {
                 self.navController.view.frame = CGRect(x: self.view.frame.width/3, y: self.navController.view.frame.minY, width: self.navController.view.frame.width, height: self.navController.view.frame.height)
             })
-        }
-        else {
+        } else {
             navigationController?.setNavigationBarHidden(false, animated: true)
             messageInputBar.isHidden = false
             UIView.animate(withDuration: 0.3, animations: {
@@ -173,18 +180,19 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
                 self.panelOut = false
             })
         }
-        
     }
 
-    func membersPressed() { //when the members button is pressed in the side panel
-        navController.pushViewController(membersView, animated: true) //the side panel nav handles it
+    // Show members VC when user clicks members option.
+    func membersPressed() {
+        navController.pushViewController(membersView, animated: true)
     }
     
+    // Show notes VC when user clicks notes option.
     func notesPressed() {
         notesView.className = className
         notesView.classId = classID
-        navController.pushViewController(notesView, animated: true)
         
+        navController.pushViewController(notesView, animated: true)
     }
     
     // Removes the user from the given class.
@@ -222,38 +230,37 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
     }
 
     
-    // sets style for status bar
+    // Sets style for status bar.
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-    // sets up basic configurations for messages collection view
+    // Sets up basic configurations for messages collection view.
     func configureMessageCollectionView() {
         messagesCollectionView.messagesDataSource = self
         
-        scrollsToBottomOnKeyboardBeginsEditing = true // default false
-        maintainPositionOnKeyboardFrameChanged = true // default false
+        scrollsToBottomOnKeyboardBeginsEditing = true // Default false.
+        maintainPositionOnKeyboardFrameChanged = true // Default false.
         
         messagesCollectionView.addSubview(refreshControl)
-        refreshControl.addTarget(self, action: #selector(loadMoreMessages), for: .valueChanged)
         
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
     }
     
-    // initializes the input bar for messages
+    // Initializes the input bar for messages.
     func configureMessageInputBar() {
         messageInputBar.delegate = self
     }
     
-    // loads messages for chat room
+    // Loads messages for chat room.
     func loadMessages() {
         // Chat room database reference.
         let databaseClass = Constants.refs.databaseChats.child(className)
         let senderId = Auth.auth().currentUser!.uid
         
         // Reads in messages from specific class and inserts them into messages list to be displayed.
-        // Observer remains active to allow new messages from other users to display
+        // Observer remains active to allow new messages from other users to display.
         let ref = Constants.refs.databaseUsers
         ref.child(senderId).observeSingleEvent(of: .value, with: { (snapshot) in
             let value = snapshot.value as? NSDictionary
@@ -282,18 +289,13 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
         }
     }
     
-    // TODO: Load more messages upon scrolling to the top of messages screen
-    @objc
-    func loadMoreMessages() {
-        
-    }
-    
     // MARK: - Helpers
     
-    // inserts message into list and updates view
+    // Inserts message into list and updates view.
     func insertMessage(_ message: Message) {
         messageList.append(message)
-        // Reload last section to update header/footer labels and insert a new one
+        
+        // Reload last section to update header/footer labels and insert a new one.
         messagesCollectionView.performBatchUpdates({
             messagesCollectionView.insertSections([messageList.count - 1])
             if messageList.count >= 2 {
@@ -306,129 +308,115 @@ final class MessageViewController: MessagesViewController, MembersDelegate, Note
         })
     }
     
-    // checks if last section is visible for scrolling to bottom
+    // Checks if last section is visible for scrolling to bottom.
     func isLastSectionVisible() -> Bool {
-        
-        guard !messageList.isEmpty else { return false }
+        guard !messageList.isEmpty else {
+            return false
+        }
         
         let lastIndexPath = IndexPath(item: 0, section: messageList.count - 1)
-        
         return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
     }
-    
 }
 
 // MARK: - MessagesDisplayDelegate
 
 extension MessageViewController: MessagesDisplayDelegate {
-    
     // MARK: - Text Messages
     
-    // selects text color depending on who sent message.
+    // Selects text color depending on who sent message.
     func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
         return isFromCurrentSender(message: message) ? .white : .darkText
     }
     
-    // sets message background color depending on who sent message
+    // Sets message background color depending on who sent message.
     func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
         return isFromCurrentSender(message: message) ? JDColor.appAccent.color : UIColor(red: 230/255, green: 241/255, blue: 253/255, alpha: 1)
     }
     
-    // sets bubble tail direction depending on who sent message
+    // Sets bubble tail direction depending on who sent message.
     func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
-        
         let tail: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
         return .bubbleTail(tail, .curved)
     }
-    
 }
 
 // MARK: - MessagesLayoutDelegate
 
 extension MessageViewController: MessagesLayoutDelegate {
-    
-    // hides extra height for avatars
+    // Hides extra height for avatars.
     func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
         return 0
     }
     
-    // sets height above message for name
+    // Sets height above message for name.
     func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
         return 20
     }
     
-    // sets height below message for time
+    // Sets height below message for time.
     func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
         return 16
     }
-    
 }
 
 extension MessageViewController: MessagesDataSource {
-    // returns the current sender
+    // Returns the current sender.
     func currentSender() -> Sender {
         return sender!
     }
     
-    // returns number of messages in chat
+    // Returns number of messages in chat.
     func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
         return messageList.count
     }
     
-    // returns message for given section
+    // Returns message for given section.
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
         return messageList[indexPath.section]
     }
     
-    // sets display name style above message
+    // Sets display name style above message.
     func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
         let name = message.sender.displayName
         return NSAttributedString(string: name, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1), NSAttributedString.Key.foregroundColor : JDColor.appSubText.color])
     }
     
-    // sets date and date style below message
+    // Sets date and date style below message.
     func messageBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
-        
         let date = message.sentDate
         let calendar = Calendar.current
         var hour = calendar.component(.hour, from: date)
         let minutes = calendar.component(.minute, from: date)
         var minutesString = "00"
-        if(minutes < 10)
-        {
+        if minutes < 10 {
             minutesString = "0\(minutes)"
-        }
-        else
-        {
+        } else {
             minutesString = "\(minutes)"
         }
+        
         var hoursString = "00"
-        if(hour < 12)
-        {
+        if hour < 12 {
             hoursString = "\(hour)"
             minutesString += " am"
-        }
-        else if (hour > 12)
-        {
+        } else if hour > 12 {
             hour -= 12
             hoursString = "\(hour)"
             minutesString += " pm"
-        }
-        else
-        {
+        } else {
             hoursString = "\(hour)"
             minutesString += " pm"
         }
+        
         let dateString = hoursString + ":" + minutesString
         return NSAttributedString(string: dateString, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2), NSAttributedString.Key.foregroundColor : JDColor.appSubText.color])
     }
 }
 
 extension MessageViewController: MessageInputBarDelegate {
-    
-    // handles message sending upon pressing send
+    // Handles message sending upon pressing send.
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
-        // generates message object and writes to database.
+        // Generates message object and writes to database.
         for component in inputBar.inputTextView.components {
             let current = currentSender()
             if let str = component as? String {
@@ -482,7 +470,8 @@ extension MessageViewController: MessageInputBarDelegate {
                 }
             }
         }
-        // refreshes message bar
+        
+        // Refresh message bar.
         inputBar.inputTextView.text = String()
         messagesCollectionView.scrollToBottom(animated: true)
     }
@@ -490,7 +479,6 @@ extension MessageViewController: MessageInputBarDelegate {
     // Handles "reply" action in message notification.
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         let identifier = response.actionIdentifier
-//        let request = response.notification.request
         
         if identifier == "reply" {
             let textResponse = response as! UNTextInputNotificationResponse
@@ -505,6 +493,4 @@ extension MessageViewController: MessageInputBarDelegate {
         
         completionHandler()
     }
-    
 }
-
